2db9143a3cde68b0c60d1cd53e02b812c8f14c7a
Brian Woodward <brian.woodward@gmail.com>
2015-06-18 18:42:16 -0400

    implementation from call with jon

--- index.js
+++ index.js
@@ -7,6 +7,53 @@
 
 'use strict';
 
-module.exports = function () {
-  // do stuff
-};
+var get = require('get-value');
+
+function recent(views, options) {
+  options = options || {};
+  var prop = options.prop || 'data.date';
+  var limit = options.limit || 10;
+
+  var res = {};
+  var keys = [];
+
+  for (var key in views) {
+    if (views.hasOwnProperty(key)) {
+      var date = createdDate(key, views[key], prop);
+      res[date] = res[date] || [];
+      res[date].push(key);
+    }
+  }
+
+  keys = Object.keys(res).sort();
+  var len = keys.length;
+  var num = 0;
+  var acc = {};
+
+  while (len-- && num < limit) {
+    var arr = res[keys[len]];
+    for (var j = 0; j < arr.length; j++) {
+      var key = arr[j];
+      acc[key] = views[key];
+      num++;
+      if (num > limit) break;
+    }
+  }
+  return acc;
+}
+
+function createdDate(key, value, prop) {
+  var val = prop ? get(value, prop) : value;
+  var str = val || key;
+
+  var re = /^(\d{4})-(\d{2})-(\d{2})/;
+  var m = re.exec(str);
+  if (!m) return null;
+  return String(m[1]) + String(m[2]) + String(m[3])
+}
+
+/**
+ * Expose `recent`
+ */
+
+module.exports = recent;
